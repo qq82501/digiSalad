33f2e551d876b32cd7533380c012342d140fb7d8
qq82501 <qq82501@gmail.com>
2023-01-18 17:27:57 +0800

    repush

--- src/context/modal-context.js
+++ src/context/modal-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 export const ModalContext = React.createContext({
   isModalOpen: false,
   openModalHandler: function () {},
